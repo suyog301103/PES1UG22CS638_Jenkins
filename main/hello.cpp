41e715626ebc06e67e6daa08599313ebfce49db6
Suyog <suyog.sampangi@gmail.com>
2025-03-13 16:01:27 +0530

    second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "The SRN is PES1UG22CS638\n";
+  cout<< "The name is Suyog Sampangi\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
